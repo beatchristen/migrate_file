83888d5fb38f5103f36d1a370c9c9a580baec9a3
Beat Christen <beat.christen@systarch.com>
2020-07-24 16:27:36 +0200

    Update the module for D9 compatibility: 3150208-4.patch

--- src/Plugin/migrate/process/FileImport.php
+++ src/Plugin/migrate/process/FileImport.php
@@ -189,15 +189,15 @@ class FileImport extends FileCopy {
 
     // Build the destination file uri (in case only a directory was provided).
     $destination = $this->getDestinationFilePath($source, $destination);
-    if (!$this->fileSystem->uriScheme($destination)) {
+    if (!StreamWrapperManagerInterface::getScheme($destination)) {
       if (empty($destination)) {
-        $destination = file_default_scheme() . '://' . preg_replace('/^\//' ,'', $destination);
+        $destination = \Drupal::config('system.file')->get('default_scheme') . '://' . preg_replace('/^\//' ,'', $destination);
       }
     }
     $final_destination = '';
 
     // If we're in re-use mode, reuse the file if it exists.
-    if ($this->getOverwriteMode() == FILE_EXISTS_ERROR && $this->isLocalUri($destination) && is_file($destination)) {
+    if ($this->getOverwriteMode() == \Drupal\Core\File\FileSystemInterface::EXISTS_ERROR && $this->isLocalUri($destination) && is_file($destination)) {
       // Look for a file entity with the destination uri.
       if ($files = \Drupal::entityTypeManager()->getStorage('file')->loadByProperties(['uri' => $destination])) {
         // Grab the first file entity with a matching uri.
@@ -288,13 +288,13 @@ class FileImport extends FileCopy {
    */
   protected function getOverwriteMode() {
     if (!empty($this->configuration['rename'])) {
-      return FILE_EXISTS_RENAME;
+      return \Drupal\Core\File\FileSystemInterface::EXISTS_RENAME;
     }
     if (!empty($this->configuration['reuse'])) {
-      return FILE_EXISTS_ERROR;
+      return \Drupal\Core\File\FileSystemInterface::EXISTS_ERROR;
     }
 
-    return FILE_EXISTS_REPLACE;
+    return \Drupal\Core\File\FileSystemInterface::EXISTS_REPLACE;
   }
 
   /**
@@ -325,7 +325,7 @@ class FileImport extends FileCopy {
   protected function getDestinationFilePath($source, $destination) {
     if ($this->isDirectory($destination)) {
       $parsed_url = parse_url($source);
-      $filepath = $destination . drupal_basename($parsed_url['path']);
+      $filepath = $destination . \Drupal\Core\File\FileSystem::basename($parsed_url['path']);
     }
     else {
       $filepath = $destination;
